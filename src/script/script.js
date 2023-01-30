16ed2e21e7003642b16ca59509c937408582adfd
Leo <l3oxon3@gmail.com>
2023-01-30 14:10:30 -0300

    Active

--- src/script/script.js
+++ src/script/script.js
@@ -1,25 +1,32 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
         const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))        
 
+var namesList = document.getElementById("namesList");
+
+var names = namesList.getElementsByTagName("a");
+
+var studies = document.getElementById("studies");
+
+studies.innerHTML += ' - ' + names.length;
+
 function filterNames() {
-    // Get the input value
     var inputValue = document.getElementById("searchInput").value;
 
-    // Get the names list
     var namesList = document.getElementById("namesList");
 
-    // Get all the names
     var names = namesList.getElementsByTagName("a");
 
-    // Loop through the names
-    for (var i = 0; i < names.length; i++) {
-        var name = names[i];
+    var studies = document.getElementById("studies");
 
-        // If the name contains the input value, show it, otherwise hide it
+    for (var i = 0; i < names.length; i++) {
+        var name = names[i];        
+        
         if (name.innerHTML.toUpperCase().includes(inputValue.toUpperCase())) {
             name.style.display = "";
         } else {
             name.style.display = "none";
         }
     }
+    
 }
+
