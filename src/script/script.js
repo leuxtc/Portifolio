a31515d7310b4161c460fc32178e86d8aa1f29b3
Leo <l3oxon3@gmail.com>
2023-02-03 00:02:23 -0300

    Learn

--- src/script/script.js
+++ src/script/script.js
@@ -30,10 +30,32 @@ function filterNames() {
     
 }
 
-varvar
+var s = 0;
+var m = 0;
+var h = 0;
+var time = 1000;
+var cron;
 
-for (var i = 0; i < 99999; i++) {
-    var count = [i]
-    var elapsed = document.getElementById("elapsed");
-    elapsed.innerHTML = "Elapsed: " + count;
+$(window).on("load", function(){
+    cron = setInterval(() => { timer(); }, time);
+});
+
+function timer(){ 
+    s++;
+
+    if (s == 60) {
+        s = 0;
+        m++; 
+
+        if (m == 60) { 
+            m = 0;
+            h++;
+        }
+    }
+    
+    var format = (h < 10 ? '0' + h : h) + ':' + (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s);
+
+    document.getElementById('elapsed').innerHTML = format + ' elapsed';
+
+    return format
 }
